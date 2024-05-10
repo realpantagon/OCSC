bc00d532e3a209f4552d9e4ca4e05c98c9f6ded7
realpantagon <pichayoothunchainao@gmail.com>
2024-05-10 11:05:35 +0700

    css order page

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Navbar from './Component/Navbar';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "./Component/Navbar";
 
 function Order() {
   const [userRecord, setUserRecord] = useState(null);
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [selectedForm, setSelectedForm] = useState(null);
 
   useEffect(() => {
-    const storedUsername = localStorage.getItem('username');
+    const storedUsername = localStorage.getItem("username");
     if (storedUsername) {
       setUsername(storedUsername);
     }
@@ -20,18 +20,21 @@ function Order() {
       try {
         setIsLoading(true);
         const response = await axios.get(
-          'https://api.airtable.com/v0/appuJ44jDsA3MjkPx/Approved%20Exhibitors',
+          "https://api.airtable.com/v0/appuJ44jDsA3MjkPx/Approved%20Exhibitors",
           {
             headers: {
-              Authorization: 'Bearer pat0OBMDNs4sQDmvL.46e6a60992296cd058398c5407b91169e9764861003a751a45e2e37c2fa4cc83',
+              Authorization:
+                "Bearer pat0OBMDNs4sQDmvL.46e6a60992296cd058398c5407b91169e9764861003a751a45e2e37c2fa4cc83",
             },
           }
         );
         const records = response.data.records;
-        const userRecord = records.find(record => record.fields.Username === username);
+        const userRecord = records.find(
+          (record) => record.fields.Username === username
+        );
         setUserRecord(userRecord);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       } finally {
         setIsLoading(false);
       }
@@ -50,43 +53,59 @@ function Order() {
     <div>
       <Navbar />
       <div className="flex">
-        <div className="w-1/4 p-4 bg-gray-200">
-          <h2 className="text-xl font-semibold mb-4">Order Forms</h2>
+        <div className="w-1/4 p-4 bg-white ">
           <ul>
             <li className="mb-2">
               <button
-                className="text-blue-500 hover:text-blue-700"
-                onClick={() => openJotForm('r1 service(Furniture)')}
+                className={`text-blue-500 hover:text-blue-700 w-full py-2 px-4 rounded-full focus:outline-none transition duration-300 ease-in-out ${
+                  selectedForm === "r1 service(Furniture)"
+                    ? "bg-blue-500 text-white shadow-lg"
+                    : "bg-white text-black"
+                }`}
+                onClick={() => openJotForm("r1 service(Furniture)")}
               >
                 Furniture Order
               </button>
             </li>
             <li className="mb-2">
               <button
-                className="text-blue-500 hover:text-blue-700"
-                onClick={() => openJotForm('r2 service(Electric)')}
+                className={`text-blue-500 hover:text-blue-700 w-full py-2 px-4 rounded-full focus:outline-none transition duration-300 ease-in-out ${
+                  selectedForm === "r2 service(Electric)"
+                    ? "bg-blue-500 text-white shadow-lg"
+                    : "bg-white text-black"
+                }`}
+                onClick={() => openJotForm("r2 service(Electric)")}
               >
                 Electric Order
               </button>
             </li>
             <li className="mb-2">
               <button
-                className="text-blue-500 hover:text-blue-700"
-                onClick={() => openJotForm('r3 service(A/V&Computer)')}
+                className={`text-blue-500 hover:text-blue-700 w-full py-2 px-4 rounded-full focus:outline-none transition duration-300 ease-in-out ${
+                  selectedForm === "r3 service(A/V&Computer)"
+                    ? "bg-blue-500 text-white shadow-lg"
+                    : "bg-white text-black"
+                }`}
+                onClick={() => openJotForm("r3 service(A/V&Computer)")}
               >
                 A/V & Computer Order
               </button>
             </li>
             <li>
               <button
-                className="text-blue-500 hover:text-blue-700"
-                onClick={() => openJotForm('Payment link')}
+                className={`text-blue-500 hover:text-blue-700 w-full py-2 px-4 rounded-full focus:outline-none transition duration-300 ease-in-out ${
+                  selectedForm === "Payment link"
+                    ? "bg-blue-500 text-white shadow-lg"
+                    : "bg-white text-black"
+                }`}
+                onClick={() => openJotForm("Payment link")}
               >
                 Payment
               </button>
             </li>
           </ul>
         </div>
+
         <div className="flex-1 p-4">
           {isLoading ? (
             <div>Loading...</div>
@@ -95,7 +114,6 @@ function Order() {
               src={userRecord.fields[selectedForm]}
               width="100%"
               height="800px"
-              frameBorder="0"
               allowFullScreen
             ></iframe>
           ) : (
