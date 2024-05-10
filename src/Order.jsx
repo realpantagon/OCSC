5006ffd81e8a6c3dee6647bfe1c5d2ba75a0648e
realpantagon <pichayoothunchainao@gmail.com>
2024-05-10 15:46:10 +0700

    spinner Loading

--- src/Order.jsx
+++ src/Order.jsx
@@ -64,9 +64,7 @@ function Order() {
                 }`}
                 onClick={() => openJotForm("r4 service(Form 1)")}
               >
-                <div className="text-left">
-                Form 1: Exhibitor badge
-                </div>
+                <div className="text-left">Form 1: Exhibitor badge</div>
               </button>
             </li>
             <li className="mb-2">
@@ -158,7 +156,33 @@ function Order() {
 
         <div className="flex-1 p-4">
           {isLoading ? (
-            <div>Loading...</div>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="flex items-center">
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 text-gray-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                <span className="text-gray-600 text-lg">
+                  Loading form data...
+                </span>
+              </div>
+            </div>
           ) : userRecord && selectedForm ? (
             <iframe
               src={userRecord.fields[selectedForm]}
