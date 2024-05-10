4105da0e85c4334875b450a3b0ee4d925ae566f1
realpantagon <pichayoothunchainao@gmail.com>
2024-05-10 14:43:09 +0700

    Update username to identify user

--- src/profile.jsx
+++ src/profile.jsx
@@ -104,125 +104,311 @@ const MainSection = ({ userRecord, openItem }) => {
       {openItem === "exhibitorProfile-generalInfo" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">General Information</h2>
-          <ul className="ml-4">
-            <li >
-              Organization Name: {userRecord.fields["Organization Name"] || "-"}
-            </li>
-            <li>
-              Street Address:{" "}
-              {userRecord.fields["Street Address (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Street Address Line 2:{" "}
-              {userRecord.fields["Street Address Line 2 (from Booth no.)"] || "-"}
-            </li>
-            <li>City: {userRecord.fields["City (from Booth no.)"] || "-"}</li>
-            <li>
-              State / Province:{" "}
-              {userRecord.fields["State / Province (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Postal / Zip Code:{" "}
-              {userRecord.fields["Postal / Zip Code (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Country: {userRecord.fields["Country (from Booth no.)"] || "-"}
-            </li>
-          </ul>
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Organization Name</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Organization Name"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Street Address</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Street Address (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Street Address Line 2</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Street Address Line 2 (from Booth no.)"] ||
+                    "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">City</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["City (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">State / Province</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["State / Province (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Postal / Zip Code</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Postal / Zip Code (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Country</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Country (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "exhibitorProfile-contactPerson" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Contact Person</h2>
-          <ul className="ml-4">
-            <li>
-              Prefix: {userRecord.fields["Prefix (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              First Name: {userRecord.fields["First Name (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Last Name: {userRecord.fields["Last Name (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Position: {userRecord.fields["Position (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Email: {userRecord.fields["Email (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Phone Number: {userRecord.fields["Phone Number (from Booth no.)"] || "-"}
-            </li>
-          </ul>
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Prefix</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Prefix (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">First Name</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["First Name (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Last Name</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Last Name (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Position</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Position (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Email</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Email (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Phone Number</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Phone Number (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "exhibitorProfile-levelOfStudies" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Level of Studies Offer</h2>
-          <ul className="ml-4">
-            <li>
-              Level of Studies Offered:{" "}
-              {userRecord.fields["Level of Studies Offered (from Booth no.)"] || "-"}
-            </li>
-          </ul>
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Level of Studies Offered</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Level of Studies Offered (from Booth no.)"] ||
+                    "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "exhibitorProfile-topMajors" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Top 10 Majors</h2>
-          {/* Render Top 10 Majors data */}
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Major
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              {[...Array(10)].map((_, index) => (
+                <tr key={index} className="hover:bg-gray-50">
+                  <td className="px-6 py-4">
+                    {userRecord.fields[`Famous #${index + 1} (from Booth no.)`] ||
+                      "-"}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "exhibitorProfile-promotion" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Promotion</h2>
-          {/* Render Promotion data */}
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Promotion Detail</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Promotion Detail (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "exhibitorProfile-scholarship" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Scholarship</h2>
-          {/* Render Scholarship data */}
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Scholarship</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Scholarship (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "exhibitorSpace" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Exhibitor Space</h2>
-          <ul>
-            <li>
-              Total Booths Required:{" "}
-              {userRecord.fields["Total Booths Required (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Institution Name on Booth Fascia:{" "}
-              {userRecord.fields["Institution name to be put on booth fascia (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              National Flag on Booth & Media for PR:{" "}
-              {userRecord.fields["National flag on booth & Media for PR (from Booth no.)"] || "-"}
-            </li>
-          </ul>
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Total Booths Required</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Total Booths Required (from Booth no.)"] ||
+                    "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Institution Name on Booth Fascia</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields[
+                    "Institution name to be put on booth fascia (from Booth no.)"
+                  ] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">National Flag on Booth & Media for PR</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields[
+                    "National flag on booth & Media for PR (from Booth no.)"
+                  ] || "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
       {openItem === "billingInfo" && (
         <div>
           <h2 className="text-2xl font-semibold mb-4">Billing Information</h2>
-          <ul>
-            <li>
-              Organization Name: {userRecord.fields["Organization Name 2 (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Street Address: {userRecord.fields["Street Address 2 (from Booth no.)"] || "-"}
-            </li>
-            <li>
-              Street Address Line 2:{" "}
-              {userRecord.fields["Street Address Line 2 2 (from Booth no.)"] || "-"}
-            </li>
-            <li>City: {userRecord.fields["City 2 (from Booth no.)"] || "-"}</li>
-            <li>
-              State / Province: {userRecord.fields["State / Province 2 (from Booth no.)"] || "-"}
-            </li>
-          </ul>
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Field
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Organization Name</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Organization Name 2 (from Booth no.)"] ||
+                    "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Street Address</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Street Address 2 (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">Street Address Line 2</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["Street Address Line 2 2 (from Booth no.)"] ||
+                    "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">City</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["City 2 (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+              <tr className="hover:bg-gray-50">
+                <td className="px-6 py-4">State / Province</td>
+                <td className="px-6 py-4">
+                  {userRecord.fields["State / Province 2 (from Booth no.)"] || "-"}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
     </div>
@@ -231,9 +417,16 @@ const MainSection = ({ userRecord, openItem }) => {
 
 function Profile() {
   const [userRecord, setUserRecord] = useState(null);
-  const [username, setUsername] = useState("2024ocscexpo111");
+  const [username, setUsername] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [openItem, setOpenItem] = useState(null);
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -249,7 +442,9 @@ function Profile() {
           }
         );
         const records = response.data.records;
-        const userRecord = records.find((record) => record.fields.Username === username);
+        const userRecord = records.find(
+          (record) => record.fields.Username === username
+        );
         setUserRecord(userRecord);
       } catch (error) {
         console.error("Error fetching user data:", error);
@@ -258,7 +453,9 @@ function Profile() {
       }
     };
 
-    fetchUserData();
+    if (username) {
+      fetchUserData();
+    }
   }, [username]);
 
   return (
