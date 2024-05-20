f4fdf23c3d7190ebf6cbb946b222b3a06c1a2e02
tanthidayu <tanthidayu_0000@hotmail.com>
2024-05-20 10:21:07 +0700

    fixed size

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                     isActive('/profile')
                       ? 'bg-red-500 text-white rounded-3xl'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
-                  } px-3 py-2 rounded-md text-sm font-medium`}
+                  } px-3 py-2 rounded-md text-lg font-medium`}
                 >
                   Profile
                 </Link>
@@ -38,7 +38,7 @@ const Navbar = () => {
                     isActive('/manual')
                       ? 'bg-red-500 text-white rounded-3xl'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
-                  } px-3 py-2 rounded-md text-sm font-medium`}
+                  } px-3 py-2 rounded-md text-lg font-medium`}
                 >
                   Manual
                 </Link>
@@ -48,7 +48,7 @@ const Navbar = () => {
                     isActive('/order')
                       ? 'bg-red-500 text-white rounded-3xl'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
-                  } px-3 py-2 rounded-md text-sm font-medium`}
+                  } px-3 py-2 rounded-md text-lg font-medium`}
                 >
                   Order
                 </Link>
@@ -58,7 +58,7 @@ const Navbar = () => {
                     isActive('/faq')
                       ? 'bg-red-500 text-white rounded-3xl'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
-                  } px-3 py-2 rounded-md text-sm font-medium`}
+                  } px-3 py-2 rounded-md text-lg font-medium`}
                 >
                   FAQ
                 </Link>
