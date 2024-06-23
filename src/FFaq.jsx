04dfef11e461ae27fc850ddeb4a39c0303469886
thidayu <tanthidayu_0000@hotmail.com>
2024-06-23 23:26:53 +0700

    FAQ stay tuned

--- src/FFaq.jsx
+++ src/FFaq.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Navbar from './Component/Navbar'
+import React from "react";
+import Navbar from "./Component/Navbar";
 
 function FFaq() {
   return (
-    <div>
-        <Navbar/>
-        Faq</div>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-1 flex items-center justify-center">
+        <img className="w-[30%] h-auto" src="FAQ.png" alt="FAQ" />
+        <h1 className="text-4xl font-bold mt-64 ml-[-64px]">Loading...</h1>
+      </div>
+    </div>
+  );
 }
 
-export default FFaq
+export default FFaq;
